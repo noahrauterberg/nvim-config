32b18f72ff54210ac2c65f3209e405292ddb435d
noah <rauterberg@campus.tu-berlin.de>
2025-08-12 19:47:20 +0200

    init

---
+++ lua/config/remap.lua
@@ -0,0 +1,31 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Move selection up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Cursos stays in place
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Preserve buffer on paste over highlighted section
+vim.keymap.set("x", "<leader>p", "\"_dp")
+-- Delete without buffer (into void buffer)
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Search and replace current file for current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlights text when yanking",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
---
+++ lua/config/set.lua
@@ -0,0 +1,32 @@
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.list = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 8
+vim.opt.showcmd = true
+vim.opt.colorcolumn = "100"
+
+vim.opt.undofile = true
+vim.opt.clipboard = "unnamed"
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- No automatic comment insertion
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+
+vim.opt.showmode = false
+
+vim.opt.termguicolors = false
+vim.cmd("colorscheme habamax")
+
