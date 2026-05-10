19fc54e66aefc585e33e8cb03410215396bd2add
noah <rauterberg@campus.tu-berlin.de>
2026-05-10 12:30:26 +0200

    update treesitter version

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,36 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
 
   config = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "go",
-            "java",
-            "javascript",
-            "lua",
-            "vim",
-            "vimdoc",
-            "c",
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-          disable = { "python", "c" } -- these and some other langs don't work well
-        },
-      })
+    local ts = require("nvim-treesitter")
+
+    ts.setup({})
+
+    ts.install({
+      "c",
+      "go",
+      "java",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "typescript",
+      "vim",
+      "vimdoc",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+
+        local ft = vim.bo[args.buf].filetype
+        if ft ~= "python" and ft ~= "c" then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
   end
 }
-
